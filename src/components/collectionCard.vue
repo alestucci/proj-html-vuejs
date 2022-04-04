03c17177edda4632fee0a7b75ee4e7dd108de375
alestucci <alessandrotucci@gmail.com>
2022-04-04 16:35:47 +0200

    Sezione Collections

---
+++ src/components/collectionCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <h1>Winter Collection</h1>
+    <h2>Stilish and Warm</h2>
+    <div class="btn">View More</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: calc(100% / 3);
+  height: 100%;
+  padding: 2rem 0;
+  background-image: url("../assets/img/winter_collection_bg.jpg");
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  text-align: center;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+</style>
---
+++ src/components/mainCollections.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="main-collections">
+    <collection-card v-for="card in cards" :key="card.title" />
+  </div>
+</template>
+
+<script>
+import CollectionCard from "./collectionCard.vue";
+export default {
+  name: "mainCollections",
+  components: {
+    CollectionCard,
+  },
+  data() {
+    return {
+      cards: [
+        {
+          title: "Winter Collection",
+          description: "Stylish and warm",
+          link: "#!",
+        },
+        {
+          title: "Spring Collection",
+          description: "Bright and colorful",
+          link: "#!",
+        },
+        {
+          title: "Autumn Collection",
+          description: "Rich and comfortable",
+          link: "#!",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#main-collections {
+  height: 600px;
+  display: flex;
+}
+</style>
